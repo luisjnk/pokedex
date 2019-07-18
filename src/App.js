29095adc7f76ff117d5d798692f62075ee7e067b
luisjnk <luisjnk@gmail.com>
2019-07-18 10:43:51 -0300

    commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Pokedex from './Pokedex';
+import Pokegame from './Pokegame';
+
+function App() {
+  return (
+    <div className="App">
+    <Pokegame/>
+    </div>
+  );
+}
+
+//<Pokedex />
+export default App;
